bce8a9a6691dcc3b75530ab3fa02696d9007a81d
BabichOlka <64680272+BabichOlka@users.noreply.github.com>
2020-07-08 12:27:13 +0300

    new

---
+++ src/main/java/myChat/dao/ClientDAO.java
@@ -0,0 +1,15 @@
+package myChat.dao;
+
+
+import myChat.model.ClientM;
+
+import java.util.List;
+
+public interface ClientDAO {
+    void create(ClientM clientM);
+    ClientM getClientByLogin(String login);
+    List<ClientM> get();
+    void update(ClientM clientM);
+    void delete(long id);
+    ClientM getSaltByLogin(String login);
+}
---
+++ src/main/java/myChat/dao/ClientDAOImpl.java
@@ -0,0 +1,56 @@
+package myChat.dao;
+
+import myChat.model.ClientM;
+import myChat.config.SessionFactory;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+public class ClientDAOImpl implements ClientDAO {
+    String resource = "mybatisconfig.xml";
+
+    private final static String namespace = "client_mapper";
+
+    public void create(ClientM clientM) {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        sqlSession.insert(namespace + ".create", clientM);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public ClientM getClientByLogin(String login) {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        ClientM clientM = sqlSession.selectOne(namespace + ".getClientByLogin", login);
+        sqlSession.close();
+        return clientM;
+    }
+
+    public List<ClientM> get() {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        List<ClientM> clientsM = sqlSession.selectList(namespace + ".get");
+        sqlSession.close();
+        return clientsM;
+    }
+
+    public void update(ClientM clientM) {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        sqlSession.update(namespace + ".update", clientM);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public void delete(long id) {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        sqlSession.delete(namespace + ".deleteById", id);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    public ClientM getSaltByLogin(String login) {
+        SqlSession sqlSession = SessionFactory.getSession(resource);
+        ClientM salt = sqlSession.selectOne(namespace + ".getSaltByLogin", login);
+        sqlSession.close();
+        return salt;
+    }
+
+}
